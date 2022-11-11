96a46ec5868b9dd5aef6c987ddf601c4be2a2070
McTello <89931817+McTello@users.noreply.github.com>
2022-11-11 11:19:31 +0100

    initial commit

--- Assignment1.py
+++ Assignment1.py
@@ -1,1 +1,10 @@
-
+value1 = int(input("Enter a number: "))
+value2 = int(input("Enter a number: "))
+sum = value1 + value2
+minus = value1 - value2
+multiple = value1 * value2
+division = value1 / value2
+print(f"The sum of {value1} and {value2} is => {sum}")
+print(f"The difference between {value1} and {value2} is => {minus}")
+print(f"The product of {value1} and {value2} is => {multiple}")
+print(f"The quotient of {value1} and {value2} is => {division}")
