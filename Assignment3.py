32f778cfcf61bb142edd222ea6984375e1caf2d4
McTello <89931817+McTello@users.noreply.github.com>
2022-11-11 13:12:03 +0100

    Third one

--- Assignment3.py
+++ Assignment3.py
@@ -1,1 +1,4 @@
-
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+department = input("Enter your department: ")
+print(f"Name is => {name}, Age => {age}, Department of => {department}")
